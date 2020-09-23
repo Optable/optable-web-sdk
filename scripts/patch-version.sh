21395be9b05c0cc2a88b49381a5fd3bca6024f18
Antoine Niek <antoineniek@gmail.com>
2020-09-23 13:38:04 -0400

    Publish npm package from buildkite

--- scripts/patch-version.sh
+++ scripts/patch-version.sh
@@ -10,12 +10,16 @@ USAGE
   exit "$exitcode"
 }
 
-if [ "$1" = "--help" ] || [ "$1" == "" ]; then
+if [ "$1" = "--help" ]; then
   usage 0
 fi
 
+if [ "$1" == "" ]; then
+  usage 1
+fi
+
 write_version() {
-  sed -i"" "s/0.0.0-REPLACE/$1/" ./package.json ./lib/build.json
+  sed -i"" "s/0.0.0-experimental/$1/" ./package.json ./lib/build.json
 }
 
 write_version "$1"
