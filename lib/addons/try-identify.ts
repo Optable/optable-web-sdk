dec55736e40886ab05be00d0649a6719fbebb23f
Antoine Niek <antoineniek@gmail.com>
2023-05-11 16:07:26 -0400

    Allow to identify from url using a custom query string parameter

--- lib/addons/try-identify.ts
+++ lib/addons/try-identify.ts
@@ -6,24 +6,26 @@ declare module "../sdk" {
   }
 }
 
+const eidRegexp = /^[a-f0-9]{64}$/i;
 function maybeValidEID(eid: string): boolean {
-  return eid.match(/^[a-f0-9]{64}$/i) !== null;
+  return eidRegexp.test(eid);
 }
 
-OptableSDK.prototype.tryIdentifyFromParams = function () {
-  const qstr = new URLSearchParams(window.location.search);
-  const keys = qstr.keys();
-  var eid: string | null = "";
-
-  for (const key of keys) {
-    if (key.match(/^oeid$/i)) {
-      eid = qstr.get(key);
+OptableSDK.prototype.tryIdentifyFromParams = function (key?: string) {
+  const keyRegexp = new RegExp(`^${key || "oeid"}$`, "i");
+  const searchParams = new URLSearchParams(window.location.search);
+  let eid = "";
+  for (const [key, value] of searchParams) {
+    if (keyRegexp.test(key)) {
+      eid = value
       break;
     }
   }
 
-  eid = eid || "";
-  if (maybeValidEID(eid)) {
-    this.identify("e:" + eid.toLowerCase());
+  if (!maybeValidEID(eid)) {
+    return
   }
+
+
+  this.identify("e:" + eid.toLowerCase());
 };
