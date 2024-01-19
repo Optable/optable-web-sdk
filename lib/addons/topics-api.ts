6a28a5bf06fe21e187af8393402b164d7c6129bd
MO-Thibault <103271673+MO-Thibault@users.noreply.github.com>
2024-01-19 18:13:17 -0500

    Create topics-api.ts

---
+++ lib/addons/topics-api.ts
@@ -0,0 +1,29 @@
+import OptableSDK from "../sdk";
+
+declare module "../sdk" {
+    export interface OptableSDK {
+        tryTopicsAPI: () => void;
+        getTopics: () => Promise<void>;
+    }
+}
+
+OptableSDK.prototype.tryTopicsAPI = async function () {
+    if (!sessionStorage.topics_fetched && 'browsingTopics' in document && document.featurePolicy.allowsFeature('browsing-topics')) {
+        const topicsArray = await document.browsingTopics();
+        if (topicsArray.length > 0) {
+            const topics: string[] = [];
+            for (let topic of topicsArray) {
+                topics.push('taxonomy version: ' + topic.taxonomyVersion + ', topic id: ' + topic.topic);
+            }
+
+            if (topics.length > 0) {
+                this.profile({
+                    topics_api: topics.join('|')
+                });
+            }
+        }
+
+        return;
+    }
+    sessionStorage.topics_fetched = true;
+}
