14d6ced2ebe3f1c0bb8a3a3d8b7bcfd9c61cc252
Nikola Milekic <45809529+Nikola-Milekic@users.noreply.github.com>
2024-01-22 16:49:51 -0500

    Add Topics Import (#79)

--- browser/sdk.ts
+++ browser/sdk.ts
@@ -4,6 +4,7 @@ import OptableSDK from "../lib/sdk";
 import "../lib/addons/gpt";
 import "../lib/addons/try-identify";
 import "../lib/addons/paapi";
+import "../lib/addons/topics-api";
 
 type OptableGlobal = {
   cmd: Commands | Function[];
