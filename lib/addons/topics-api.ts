46911a5e59c8c01ea98ae67c3551c81ede606974
MO-Thibault <103271673+MO-Thibault@users.noreply.github.com>
2024-01-22 10:38:28 -0500

    Update topics-api.ts

--- lib/addons/topics-api.ts
+++ lib/addons/topics-api.ts
@@ -10,7 +10,7 @@ declare module "../sdk" {
 declare global {
     interface Document {
         browsingTopics: () => Promise<any[]>;
-        featurePolicy:{allowsFeature: (string) => any};
+        featurePolicy:{allowsFeature: (feature:string) => any};
     }
 }
 
