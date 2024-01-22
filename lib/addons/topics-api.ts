c92c20c3ca96f8dae39f1813851ccf6c799477d4
MO-Thibault <103271673+MO-Thibault@users.noreply.github.com>
2024-01-22 11:33:04 -0500

    Update topics-api.ts

--- lib/addons/topics-api.ts
+++ lib/addons/topics-api.ts
@@ -2,39 +2,37 @@ import OptableSDK from "../sdk";
 
 declare module "../sdk" {
     export interface OptableSDK {
-        tryTopicsAPI: () => void;
+        getTopics: () => void;
     }
 }
 
 declare global {
     interface Document {
-        browsingTopics: () => Promise<Array<{
+        browsingTopics?: () => Promise<Array<{
             configVersion: string;
             modelVersion: string;
             taxonomyVersion: string;
             topic: number;
             version: string;
         }>>;
-        featurePolicy:{ allowsFeature: (feature:string) => boolean };
+        featurePolicy?: { allowsFeature: (feature: string) => boolean };
     }
 }
 
-OptableSDK.prototype.tryTopicsAPI = async function () {
-    if (!sessionStorage.topics_fetched && 'browsingTopics' in document && document.featurePolicy.allowsFeature('browsing-topics')) {
-        const topicsArray = await document.browsingTopics();
-        if (topicsArray.length > 0) {
-            const topics: string[] = [];
-            for (let topic of topicsArray) {
-                topics.push('taxonomy version: ' + topic.taxonomyVersion + ', topic id: ' + topic.topic);
-            }
-
-            if (topics.length > 0) {
-                this.profile({
-                    topics_api: topics.join('|')
-                });
-            }
+OptableSDK.prototype.getTopics = async function () {
+    if (!sessionStorage.topics_fetched &&
+        typeof document.browsingTopics === 'function' &&
+        typeof document.featurePolicy === 'object' &&
+        document.featurePolicy.allowsFeature === 'function' &&
+        document.featurePolicy.allowsFeature('browsing-topics')) {
+        const topics = (await document.browsingTopics()).map(topic => `taxonomy version ${topic.taxonomyVersion}, topic ${topic.topic}`);
+        if (topics.length > 0) {
+            this.profile({
+                topics_api: topics.join('|')
+            });
         }
     }
+
     sessionStorage.topics_fetched = true;
     return;
 }
