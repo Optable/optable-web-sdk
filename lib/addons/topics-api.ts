38c1344ca41497c2786967f5df6dca453d21d787
Antoine Niek <antoineniek@gmail.com>
2024-02-08 12:44:10 -0500

    Inject gateway frame to obtain topics from web-sdk

--- lib/addons/topics-api.ts
+++ lib/addons/topics-api.ts
@@ -1,27 +1,35 @@
 import OptableSDK from "../sdk";
+interface BrowsingTopic {
+  configVersion: string;
+  modelVersion: string;
+  taxonomyVersion: string;
+  topic: number;
+  version: string;
+}
 
 declare module "../sdk" {
+
   export interface OptableSDK {
-    getTopics: () => Promise<void>;
+    getTopics: () => Promise<BrowsingTopic[]>;
   }
 }
 
 /*
  * getTopics injects an iframe into the page that obtains the browsingTopics observed by optable.
  */
-OptableSDK.prototype.getTopics = async function() {
+OptableSDK.prototype.getTopics = async function(): Promise<BrowsingTopic[]> {
   const siteConfig = await this.site();
   if (!siteConfig.getTopicsURL) {
     throw ("origin not enabled for topics api");
   }
   const getTopicsURL = new URL(siteConfig.getTopicsURL);
   const topicsFrame = document.createElement("iframe");
-  topicsFrame.src = pixelURL.toString()
+  topicsFrame.src = getTopicsURL.toString()
   topicsFrame.allow = "browsing-topics " + getTopicsURL.origin;
   topicsFrame.style.display = "none";
 
-  const topicsPromise = new Promise<void>((resolve, reject) => {
-    window.addEventListener("message", (event: any) => {
+  const topicsPromise = new Promise<BrowsingTopic[]>((resolve, reject) => {
+    window.addEventListener("message", (event: MessageEvent<{ error?: Error, result: BrowsingTopic[] }>) => {
       if (event.source !== topicsFrame.contentWindow) {
         return
       }
