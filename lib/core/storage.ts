cd26f19c21520e6a5074395b2ce2d2a43700e657
Antoine Niek <antoineniek@gmail.com>
2024-02-08 14:46:34 -0500

    Drop googletag secure signal cache mangling

--- lib/core/storage.ts
+++ lib/core/storage.ts
@@ -46,7 +46,7 @@ class LocalStorage {
         //
         // Starting v2 this is returned in the targeting payload directly
         rtb_segtax: 5001,
-        ids: [].concat(...[values as any]).map((id: any) => ({id: String(id)})),
+        ids: [].concat(...[values as any]).map((id: any) => ({ id: String(id) })),
       }
     })
 
@@ -86,13 +86,6 @@ class LocalStorage {
       return
     }
 
-    // Invalidate the secure signals provider cache as it's unclear
-    // for how long the secure signals are cached and while LMPID have a 45 days lifetime receivers
-    // may decide to implement short PID lifetimes.
-    //
-    // HACK: Delete GPT secure signals cache directly for lmpidProvider, this is undocumented and could
-    // break in the future.
-    window.localStorage.removeItem(`_GESPSK-${lmpidProvider}`);
     window.localStorage.setItem(this.lmpidKey, provider.ids?.[0]?.id ?? "");
   }
 
