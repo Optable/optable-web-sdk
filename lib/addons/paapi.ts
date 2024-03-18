f3d8882004d910b53f74fba225cc4c531c1fff70
Antoine Niek <antoine.niek@optable.co>
2024-03-18 13:12:37 -0400

    Use googletag command queue when using installGPTAuctionConfigs (#101)

--- lib/addons/paapi.ts
+++ lib/addons/paapi.ts
@@ -25,20 +25,20 @@ interface HTMLFencedFrameElement extends HTMLElement {
 /*
  * auctionConfigFromCache obtains the cached auction configuration for the current origin
  */
-OptableSDK.prototype.auctionConfigFromCache = function(): AuctionConfig | null {
+OptableSDK.prototype.auctionConfigFromCache = function (): AuctionConfig | null {
   const siteConfig = this.siteFromCache();
   if (!siteConfig) {
-    return null
+    return null;
   }
 
   return siteConfig.auctionConfig ?? null;
-}
+};
 
 function elementInnerSize(element: HTMLElement): Size {
   const style = window.getComputedStyle(element, null);
   return {
-    width: style.getPropertyValue('width'),
-    height: style.getPropertyValue('height'),
+    width: style.getPropertyValue("width"),
+    height: style.getPropertyValue("height"),
   };
 }
 
@@ -47,56 +47,61 @@ function elementInnerSize(element: HTMLElement): Size {
  * and installs it into the page GPT slots, optionally filtered.
  */
 
-OptableSDK.prototype.installGPTAuctionConfigs = function(filter?: GPTSlotFilter) {
-  if (!window.googletag) {
-    throw ("googletag not found");
-  }
-  let slots = window.googletag.pubads().getSlots()
-  if (filter) {
-    slots = slots.filter(filter)
-  }
+OptableSDK.prototype.installGPTAuctionConfigs = function (filter?: GPTSlotFilter) {
+  const sdk = this;
+  sdk.installGPTAuctionConfigs = function () {};
 
-  const siteAuctionConfig = this.auctionConfigFromCache();
-  if (!siteAuctionConfig) {
-    return
-  }
+  window.googletag = window.googletag || { cmd: [] };
+  const gpt = window.googletag;
 
-  for (const slot of slots) {
-    const sizes = slot.getSizes();
-    const componentAuction = [];
-
-    for (const size of sizes) {
-      if (size === "fluid") {
-        continue
-      }
-
-      componentAuction.push({
-        configKey: siteAuctionConfig.seller + "-" + size.getWidth() + "x" + size.getHeight(),
-        auctionConfig: {
-          ...siteAuctionConfig,
-          requestedSize: { width: size.getWidth() + "px", height: size.getHeight() + "px" },
-        },
-      });
+  gpt.cmd.push(function () {
+    let slots = gpt.pubads().getSlots();
+    if (filter) {
+      slots = slots.filter(filter);
     }
 
-    // @ts-ignore // outdated typings for componentAuction expects some legacy field names
-    slot.setConfig({ componentAuction })
-  }
-}
+    const siteAuctionConfig = sdk.auctionConfigFromCache();
+    if (!siteAuctionConfig) {
+      return;
+    }
+
+    for (const slot of slots) {
+      const sizes = slot.getSizes();
+      const componentAuction = [];
+
+      for (const size of sizes) {
+        if (size === "fluid") {
+          continue;
+        }
+
+        componentAuction.push({
+          configKey: siteAuctionConfig.seller + "-" + size.getWidth() + "x" + size.getHeight(),
+          auctionConfig: {
+            ...siteAuctionConfig,
+            requestedSize: { width: size.getWidth() + "px", height: size.getHeight() + "px" },
+          },
+        });
+      }
+
+      // @ts-ignore // outdated typings for componentAuction expects some legacy field names
+      slot.setConfig({ componentAuction });
+    }
+  });
+};
 
 /*
  * runAdAuction runs an ad auction locally for a given spot dom ID.
  */
-OptableSDK.prototype.runAdAuction = async function(domID: string): Promise<void> {
+OptableSDK.prototype.runAdAuction = async function (domID: string): Promise<void> {
   const spot = document.getElementById(domID);
   if (!spot) {
-    throw ("spot not found");
+    throw "spot not found";
   }
   const requestedSize = elementInnerSize(spot);
 
   const siteAuctionConfig = this.auctionConfigFromCache();
   if (!siteAuctionConfig) {
-    return
+    return;
   }
 
   const auctionConfig = {
@@ -104,45 +109,45 @@ OptableSDK.prototype.runAdAuction = async function(domID: string): Promise<void>
     requestedSize,
     resolveToConfig: true,
   };
-  const fencedFrameConfig = await navigator.runAdAuction(auctionConfig)
+  const fencedFrameConfig = await navigator.runAdAuction(auctionConfig);
   if (!fencedFrameConfig) {
     spot.replaceChildren();
-    return
+    return;
   }
 
   const fencedFrame = document.createElement("fencedframe") as HTMLFencedFrameElement;
   fencedFrame.config = fencedFrameConfig;
   fencedFrame.style.border = "none";
   spot.replaceChildren(fencedFrame);
-}
+};
 
 /*
  * joinAdInterestGroups injects an iframe into the page that tags the device into the matching audiences interest group.
  */
-OptableSDK.prototype.joinAdInterestGroups = async function() {
+OptableSDK.prototype.joinAdInterestGroups = async function () {
   const siteConfig = await this.site();
   if (!siteConfig.interestGroupPixel) {
-    throw ("origin not enabled for protected audience apis");
+    throw "origin not enabled for protected audience apis";
   }
   const pixelURL = new URL(siteConfig.interestGroupPixel);
   const pixel = document.createElement("iframe");
-  pixel.src = pixelURL.toString()
+  pixel.src = pixelURL.toString();
   pixel.allow = "join-ad-interest-group " + pixelURL.origin;
   pixel.style.display = "none";
 
   const joinPromise = new Promise<void>((resolve, reject) => {
     window.addEventListener("message", (event: any) => {
       if (event.source !== pixel.contentWindow) {
-        return
+        return;
       }
 
       if (event.data.result === "success") {
-        resolve()
-        return
+        resolve();
+        return;
       }
-      reject()
-    })
-  })
+      reject();
+    });
+  });
 
   document.body.appendChild(pixel);
   return joinPromise;
