4cb64660c2e64ba2d3106254fad92568e9497ffc
Antoine Niek <antoineniek@gmail.com>
2025-01-23 15:47:03 -0500

    Support obtaining consent from cmpapi through iframe postmessage

--- lib/core/regs/tcf/cmpapi.ts
+++ lib/core/regs/tcf/cmpapi.ts
@@ -61,4 +61,81 @@ type TCData = {
 
 type AddEventListenerCallback = (data: TCData, success: boolean) => void;
 
-export { TCData };
+// installFrameProxy is a helper function that attempts to install
+// a proxy for the CMP API. This is required when the CMP API is installed
+// in a parent frame (eg when the SDK is loaded in an iframe).
+// See https://github.com/InteractiveAdvertisingBureau/GDPR-Transparency-and-Consent-Framework/blob/master/TCFv2/IAB%20Tech%20Lab%20-%20CMP%20API%20v2.md#using-postmessage
+//
+// To do so, this function looks for a ancestor frame named __tcfapiLocator and communicates
+// with it using postMessage by mocking window.__tcfapi.
+function installFrameProxy() {
+  // Already there, might be installed by ourselves or a CMP script
+  if (typeof window.__tcfapi === "function") {
+    return;
+  }
+
+  let cmpFrame;
+  const cmpCallbacks: Record<string, AddEventListenerCallback> = {};
+
+  let frame = window;
+  while (frame) {
+    // throws a reference error if no frames exist
+    try {
+      // @ts-ignore
+      if (frame.frames["__tcfapiLocator"]) {
+        cmpFrame = frame;
+        break;
+      }
+    } catch {
+      // ignore
+    }
+
+    if (frame === window.top) {
+      break;
+    }
+    // @ts-ignore
+    frame = frame.parent;
+  }
+
+  // No CMP frame found
+  if (!cmpFrame) {
+    return;
+  }
+
+  // Install the API that forwards to the CMP frame
+  window.__tcfapi = function (command, version, callback) {
+    const callId = Math.random() + "";
+    cmpCallbacks[callId] = callback;
+
+    cmpFrame.postMessage({ __tcfapiCall: { command, version, callId } }, "*");
+  };
+
+  // Receive responses
+  window.addEventListener(
+    "message",
+    (event) => {
+      let json: { __tcfapiReturn?: { callId: string; returnValue: TCData; success: boolean } } = {};
+      try {
+        json = typeof event.data === "string" ? JSON.parse(event.data) : event.data;
+      } catch {
+        // ignore
+      }
+
+      const payload = json.__tcfapiReturn;
+      if (!payload) {
+        return;
+      }
+
+      if (typeof cmpCallbacks[payload.callId] === "function") {
+        // Call the callback
+        cmpCallbacks[payload.callId](payload.returnValue, payload.success);
+        // Remove the callback
+        delete cmpCallbacks[payload.callId];
+      }
+    },
+    false
+  );
+}
+
+export type { TCData };
+export { installFrameProxy };
