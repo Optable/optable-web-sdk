9b6359a1bd434a3a002d3ed6c5cd67da6b741132
Antoine Niek <antoineniek@gmail.com>
2025-02-03 16:33:06 -0500

    Add a sid param to all calls

--- lib/sdk.test.ts
+++ lib/sdk.test.ts
@@ -60,6 +60,7 @@ describe("cid", () => {
 const defaultConfig = {
   host: TEST_HOST,
   site: TEST_SITE,
+  sessionID: "session",
 };
 
 describe("Breaking change detection: if typescript complains or a test fails it's likely a breaking change has occurred.", () => {
@@ -224,6 +225,7 @@ describe("behavior testing of", () => {
       initPassport: false,
       readOnly: false,
       experiments: [],
+      sessionID: "session",
     });
     await sdk["init"];
     expect(localStorage.setItem).toBeCalledTimes(0);
@@ -234,7 +236,7 @@ describe("behavior testing of", () => {
       expect.objectContaining({
         method: "POST",
         _bodyText: '["c:a1a335b8216658319f96a4b0c718557ba41dd1f5"]',
-        url: `${TEST_BASE_URL}/identify?osdk=web-0.0.0-experimental&cookies=no&passport=`,
+        url: `${TEST_BASE_URL}/identify?osdk=web-0.0.0-experimental&sid=session&cookies=no&passport=`,
       })
     );
 
@@ -244,7 +246,7 @@ describe("behavior testing of", () => {
       expect.objectContaining({
         method: "POST",
         _bodyText: '["c:a1a335b8216658319f96a4b0c718557ba41dd1f6"]',
-        url: `${TEST_BASE_URL}/identify?osdk=web-0.0.0-experimental&cookies=no&passport=PASSPORT`,
+        url: `${TEST_BASE_URL}/identify?osdk=web-0.0.0-experimental&sid=session&cookies=no&passport=PASSPORT`,
       })
     );
   });
@@ -263,6 +265,7 @@ describe("behavior testing of", () => {
       initPassport: true,
       readOnly: false,
       experiments: [],
+      sessionID: "session",
     });
     await sdk["init"];
     expect(window.localStorage.setItem).toHaveBeenLastCalledWith(
@@ -274,7 +277,7 @@ describe("behavior testing of", () => {
       expect.objectContaining({
         method: "GET",
         bodyUsed: false,
-        url: expect.stringContaining("config?osdk=web-0.0.0-experimental&cookies=yes"),
+        url: expect.stringContaining("config?osdk=web-0.0.0-experimental&sid=session&cookies=yes"),
       })
     );
 
@@ -284,7 +287,7 @@ describe("behavior testing of", () => {
       expect.objectContaining({
         method: "POST",
         _bodyText: '["c:a1a335b8216658319f96a4b0c718557ba41dd1f5"]',
-        url: `${TEST_BASE_URL}/identify?osdk=web-0.0.0-experimental&cookies=yes`,
+        url: `${TEST_BASE_URL}/identify?osdk=web-0.0.0-experimental&sid=session&cookies=yes`,
       })
     );
   });
