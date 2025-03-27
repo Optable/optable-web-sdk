3aa6a533d4c2c7a8af5e0205e7c26252bb61c68b
Jean-Marc Prud'homme <42589969+jmarc101@users.noreply.github.com>
2025-03-27 11:38:28 -0400

    Move origin to query params

--- lib/test/mocks.ts
+++ lib/test/mocks.ts
@@ -1,5 +1,5 @@
 const TEST_HOST = "hostmock.com";
 const TEST_SITE = "site";
-const TEST_BASE_URL = `https://${TEST_HOST}/${TEST_SITE}`;
+const TEST_BASE_URL = `https://${TEST_HOST}`;
 
 export { TEST_HOST, TEST_SITE, TEST_BASE_URL };
