d8d1bb055c2d283078ec67991bd921d5c8355efe
mosherBT <brennan.mosher@optable.co>
2026-09-25 13:05:36 -0400

    comments

--- lib/core/ois.ts
+++ lib/core/ois.ts
@@ -7,7 +7,7 @@ const oisChangeEventName = "optable-ois:change";
 
 // A custom header makes a request non-simple, so sending it where the node
 // derives no id buys a CORS preflight for nothing.
-const HEADER_PATHS = new Set(["/identify", "/sync", "/uid2/token", "/profile", "/v2/targeting"]);
+const HEADER_PATHS = new Set(["/identify", "/uid2/token", "/profile", "/v2/targeting"]);
 
 function derivesOISID(pathname: string): boolean {
   return HEADER_PATHS.has(pathname);
